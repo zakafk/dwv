4cdc4fd0710241fcbd83d13641eddf01db6e5a20
Vladimir Tolstykh <mr.tolstykh.va@gmail.com>
2024-04-21 17:12:50 +0300

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,7 +12,7 @@ export const Header: FC = () => {
   return (
     <header className="header">
       <Logo />
-      {isDesktop ? <Navigation/> : <MobileNavigation isDesktop={isDesktop} />}
+      {isDesktop ? <Navigation/> : <MobileNavigation />}
     </header>
   );
 };
