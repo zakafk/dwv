edc862e9ae79b6329502fb3af5858486b1e93655
Vladimir Tolstykh <mr.tolstykh.va@gmail.com>
2024-04-21 18:04:03 +0300

    add basename

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dwv">
       <MainLayout>
         <App />
         <Footer />
