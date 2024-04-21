555bb577a897953fca4974c44e4cb2575f168f74
digital-world-visions <167681613+digital-world-visions@users.noreply.github.com>
2024-04-21 15:53:40 +0300

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './components/App/App';
+import { MainLayout } from './components/MainLayout';
+import { Footer } from './components/Footer';
+
+import './main.scss';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <MainLayout>
+        <App />
+        <Footer />
+      </MainLayout>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
