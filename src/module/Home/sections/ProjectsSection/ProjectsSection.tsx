7d339f5100f17c79c6a2ef1ab03bc58b2fd94e2e
Vladimir Tolstykh <mr.tolstykh.va@gmail.com>
2024-04-25 00:40:26 +0300

    add: mobile design for projects section

--- src/module/Home/sections/ProjectsSection/ProjectsSection.tsx
+++ src/module/Home/sections/ProjectsSection/ProjectsSection.tsx
@@ -6,12 +6,15 @@ import { AppCarousel } from '../../../../components/AppCarousel';
 import { Button } from '../../../../components/Button';
 
 import './projectsSection.scss';
+import {useDevice} from "../../../../hooks/useDevice";
 
 export const ProjectsSection: FC = () => {
+  const { isMobile } = useDevice();
+
   return (
     <Section className="home-section home-section-projects">
       <header className="header">
-        <Rings type="gold" className="header-rings" />
+        <Rings type="gold" className="header-rings" rotate={isMobile ? 180 : 0} scale={isMobile ? 0.5 : 1} />
         <div className="header-titles">
           <h3 className="header-title">Our Innovative Projects</h3>
           <p className="header-subtitle">constantly pushing the boundaries and driving positive change</p>
@@ -20,7 +23,7 @@ export const ProjectsSection: FC = () => {
       <AppCarousel />
       <div className="footer">
         <Button text="Learn more" />
-        <Rings type="gold" className="footer-rings" rotate={-90} />
+        {!isMobile && <Rings type="gold" className="footer-rings" rotate={-90}/>}
       </div>
     </Section>
   );
--- src/module/Home/sections/ServicesSection/components/ServicesHeader/ServicesHeader.tsx
+++ src/module/Home/sections/ServicesSection/components/ServicesHeader/ServicesHeader.tsx
@@ -1,16 +1,19 @@
 import { FC } from 'react';
 
 import { Rings } from '../../../../../../components/Rings';
+import { useDevice } from '../../../../../../hooks/useDevice';
 
 import './servicesHeader.scss';
 
 export const ServicesHeader: FC = () => {
+  const { isMobile } = useDevice();
+
   return (
     <>
       <div className="services-header-img">
         <h2 className="services-header-title">Our services</h2>
         <div className="services-header-rings">
-          <Rings />
+          <Rings scale={isMobile ? 0.5 : 1} />
         </div>
       </div>
       <div className="services-header-text">
