2618619f831a48b40e29295d5933276f83ec6d42
Tolstykh Vladimir <40518956+tolstykh-va@users.noreply.github.com>
2024-04-28 14:12:59 +0400

    add: header refactoring (#12)

--- src/module/AboutUs/AboutUs.tsx
+++ src/module/AboutUs/AboutUs.tsx
@@ -3,8 +3,13 @@ import { FC } from 'react';
 import { Section } from '../../components/Section';
 import { Header } from '../../components/Header';
 
+import './aboutUs.scss';
+
 export const AboutUs: FC = () => (
-  <Section>
-    <Header />
-  </Section>
+  <>
+    <Header type="dark" />
+    <Section>
+      <div>content</div>
+    </Section>
+  </>
 );
