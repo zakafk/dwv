73973edb187048969539f1beb0d433303802dd0a
Tolstykh Vladimir <40518956+tolstykh-va@users.noreply.github.com>
2024-05-04 13:59:28 +0400

    update: service cards (#35)

--- src/module/Home/sections/ServicesSection/components/ServicesCards/Card/Modal.tsx
+++ src/module/Home/sections/ServicesSection/components/ServicesCards/Card/Modal.tsx
@@ -4,23 +4,23 @@ import { Picture } from '../../../../../../../components/Picture';
 import { AppModal } from '../../../../../../../components/AppModal';
 import { useDevice } from '../../../../../../../hooks/useDevice';
 import dots_gold from '../../../../../../../assets/dots_gold.svg';
-import circle from '../../../assets/сircle.svg';
 
 interface IModalProps {
   isVisible: boolean;
   handleHide: VoidFunction;
   title: string;
   description: string;
+  icon: string;
 }
 
-export const Modal: FC<IModalProps> = ({ isVisible, handleHide, title, description }) => {
+export const Modal: FC<IModalProps> = ({ isVisible, handleHide, title, description, icon }) => {
   const { isMobile } = useDevice();
 
   return (
   <AppModal show={isVisible} onHide={handleHide} className="services-card-modal">
     {!isMobile && <Picture src={dots_gold} className="dots"/>}
     <div className="services-card-logo">
-      <Picture src={circle}/>
+      <Picture src={icon}/>
     </div>
     <div className="titles">
       <h3 className="titles-title">{title}</h3>
