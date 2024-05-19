ea876c4ea5f04c81dcb2e9d663e03dda5581617e
Vladimir Tolstykh <mr.tolstykh.va@gmail.com>
2024-05-19 21:18:00 +0300

    fix: co-founder message

--- src/module/AboutUs/AboutUs.tsx
+++ src/module/AboutUs/AboutUs.tsx
@@ -14,9 +14,10 @@ export const AboutUs: FC = () => (
       name="George Amanatidze"
       role="General manager"
       src={george}
-      message="Digital World is the destination and the way for our generation  to build a bright future. Through digital
-        transformation, we endeavor to rethink and reshape our processes in a way that is in line with our
-        environmental commitments, and  the core objective of Sustainability."
+      message="I bring a wealth of experience as an IT Manager with a strong
+      commitment to sustainability and a keen interest in emerging technologies. My background includes managing
+      intricate projects in system development and implementation. Currently, I am pursuing an Executive MBA
+      at HULT International Business School's Dubai campus."
       link="https://www.linkedin.com/in/george-amanatidze-a24638207"
     />
     <TeamMemberSection
