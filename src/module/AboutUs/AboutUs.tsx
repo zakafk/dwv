8d85ad512a50f6865b26cdd75e4baa9054db9b7f
Tolstykh Vladimir <40518956+tolstykh-va@users.noreply.github.com>
2024-05-19 22:30:40 +0400

    fix: messages (#42)

--- src/module/AboutUs/AboutUs.tsx
+++ src/module/AboutUs/AboutUs.tsx
@@ -14,20 +14,18 @@ export const AboutUs: FC = () => (
       name="George Amanatidze"
       role="General manager"
       src={george}
-      message="I bring a wealth of experience as an IT Manager with a strong
-      commitment to sustainability and a keen interest in emerging technologies. My background includes managing
-      intricate projects in system development and implementation. Currently, I am pursuing an Executive MBA
-      at HULT International Business School's Dubai campus."
+      message="Digital World is the destination and the way for our generation  to build a bright future. Through digital
+        transformation, we endeavor to rethink and reshape our processes in a way that is in line with our
+        environmental commitments, and  the core objective of Sustainability."
       link="https://www.linkedin.com/in/george-amanatidze-a24638207"
     />
     <TeamMemberSection
       name="Anastasia Amanatidze"
       role="Co-founder"
       src={anastasia}
-      message="Anastasia brings a wealth of experience as an IT Manager with a strong
-      commitment to sustainability and a keen interest in emerging technologies. Her background includes managing
-      intricate projects in system development and implementation. Currently , Anastasia is pursuing an Executive MBA
-      at HULT International Business School's Dubai campus."
+      message="I bring a wealth of experience as an IT Manager with a strong commitment to sustainability and a keen
+      interest in emerging technologies. My background includes managing intricate projects in system development and
+      implementation. Currently, I am pursuing an Executive MBA at HULT International Business School's Dubai campus."
       position="right"
       link="https://www.linkedin.com/in/anastasiaamanatidze"
     />
