a86db16e9cd9497e30c90d7e7cdde0284589ebd4
Vladimir Tolstykh <mr.tolstykh.va@gmail.com>
2024-05-19 21:13:11 +0300

    add: co-founder info

--- src/module/AboutUs/sections/TeamMemberSection/Member/Member.tsx
+++ src/module/AboutUs/sections/TeamMemberSection/Member/Member.tsx
@@ -1,20 +1,21 @@
 import { FC } from 'react';
+import classNames from 'classnames';
 
 import { Picture } from '../../../../../components/Picture';
 import linkedin from '../../../../../assets/social/linkedin.svg';
 import { TPosition } from '../TeamMemberSection';
 
 import './member.scss';
-import classNames from "classnames";
 
 interface IMemberProps {
   name: string;
   role: string;
   src: string;
   position?: TPosition;
+  link?: string;
 }
 
-export const Member: FC<IMemberProps> = ({ name, role, src, position = 'left' }) => {
+export const Member: FC<IMemberProps> = ({ name, role, src, position = 'left', link = '#' }) => {
   return (
     <div className="member">
       <Picture src={src} className="main-img" />
@@ -24,7 +25,9 @@ export const Member: FC<IMemberProps> = ({ name, role, src, position = 'left' })
             <p className="name">{name}</p>
             <p className="role">{role}</p>
           </div>
-          <Picture src={linkedin} className="linkedin" />
+          <a href={link} className="link" target="_blank">
+            <Picture src={linkedin} className="linkedin" />
+          </a>
         </div>
       </div>
     </div>
