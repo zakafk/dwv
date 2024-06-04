49f704af4c74943c558b13f9221ab2a1354e8d37
Vladimir Tolstykh <mr.tolstykh.va@gmail.com>
2024-06-04 18:28:19 +0300

    add: current social

--- src/components/Footer/SocialSection/SocialSection.tsx
+++ src/components/Footer/SocialSection/SocialSection.tsx
@@ -2,18 +2,20 @@ import { FC } from 'react';
 
 import twitter_src from '../../../assets/social/twitter.svg';
 import instagram_src from '../../../assets/social/instagram.svg';
-import youtube_src from '../../../assets/social/youtube.svg';
-import { INSTAGRAM_URL, TWITTER_URL, YOUTUBE_URL } from '../../../constants/urls';
+import facebook_src from '../../../assets/social/facebook.svg';
+import linkedin_src from '../../../assets/social/linkedin_gold.svg';
+import { INSTAGRAM_URL, TWITTER_URL, LINKEDIN_URL, FACEBOOK_URL } from '../../../constants/urls';
 import { Logo } from '../../Logo';
 import { Link } from './Link';
 
 export const SocialSection: FC = () => (
   <>
     <Logo size={20} />
-    <div className="social d-none">
-      <Link src={twitter_src} href={TWITTER_URL} target="_blank" />
-      <Link src={instagram_src} href={INSTAGRAM_URL} target="_blank"/>
-      <Link src={youtube_src} href={YOUTUBE_URL} target="_blank" />
+    <div className="social">
+      <Link src={twitter_src} href={TWITTER_URL} target="_blank"  className="d-none"/>
+      <Link src={facebook_src} href={FACEBOOK_URL} target="_blank" />
+      <Link src={linkedin_src} href={LINKEDIN_URL} target="_blank" />
+      <Link src={instagram_src} href={INSTAGRAM_URL} target="_blank" />
     </div>
   </>
 );
