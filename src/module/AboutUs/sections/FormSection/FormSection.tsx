2ae2ba2d662e553aec181495492c20acd049ee8c
Zaqro Gogolidze <gogolidze.zaqro@gmail.com>
2024-07-22 21:58:44 +0400

    fix: contact form

--- src/module/AboutUs/sections/FormSection/FormSection.tsx
+++ src/module/AboutUs/sections/FormSection/FormSection.tsx
@@ -1,20 +1,21 @@
-import { FC, useCallback, useState, useRef, useEffect } from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import classNames from 'classnames';
-import { useLocation } from 'react-router-dom';
+import { FC, useCallback, useState, useRef, useEffect } from "react";
+import { useForm, Controller } from "react-hook-form";
+import classNames from "classnames";
+import { useLocation } from "react-router-dom";
 // import axios from 'axios';
 
-import { Section } from '../../../../components/Section';
-import { Button } from '../../../../components/Button';
-import { BaseInput } from '../../../../components/Inputs';
-import { EFields } from '../../../../enums/EFields';
-import { Rings } from '../../../../components/Rings';
-import { useDevice } from '../../../../hooks/useDevice';
+import { Section } from "../../../../components/Section";
+import { Button } from "../../../../components/Button";
+import { BaseInput } from "../../../../components/Inputs";
+import { EFields } from "../../../../enums/EFields";
+import { Rings } from "../../../../components/Rings";
+import { useDevice } from "../../../../hooks/useDevice";
 
-import './formSection.scss';
+import "./formSection.scss";
 
 export const FormSection: FC = () => {
   const { hash } = useLocation();
+  const [message, setMessage] = useState("");
   const [isFetch, setFetch] = useState<boolean>(false);
   const [isSuccess, setSuccess] = useState<boolean>(false);
   const [isError, setError] = useState<boolean>(false);
@@ -23,10 +24,10 @@ export const FormSection: FC = () => {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if(!ref?.current || hash !== '#form') return;
+    if (!ref?.current || hash !== "#form") return;
 
     setTimeout(() => {
-      ref?.current?.scrollIntoView({ block: 'start' });
+      ref?.current?.scrollIntoView({ block: "start" });
     }, 100);
   }, [ref]);
 
@@ -38,53 +39,108 @@ export const FormSection: FC = () => {
     watch,
   } = useForm({
     defaultValues: {
-      [EFields.NAME]: '',
-      [EFields.COMPANY_NAME]: '',
-      [EFields.EMAIL]: '',
-      [EFields.PHONE]: '',
+      [EFields.NAME]: "",
+      [EFields.COMPANY_NAME]: "",
+      [EFields.EMAIL]: "",
+      [EFields.PHONE]: "",
       [EFields.WHATSAPP]: false,
-      [EFields.MESSAGE]: '',
+      [EFields.MESSAGE]: "",
       [EFields.AGREEMENT]: false,
     },
   });
 
-  const [isWhatsApp, isAgreement] = watch([EFields.WHATSAPP, EFields.AGREEMENT]);
+  const [isWhatsApp, isAgreement] = watch([
+    EFields.WHATSAPP,
+    EFields.AGREEMENT,
+  ]);
 
   const isValidationError = !!Object.values(errors).length;
 
-  const onSubmit = useCallback(async (data: any) => {
-    setFetch(true);
+  const onSubmit = useCallback(async (data: any, e: any) => {
+    const web3FormsApiKeyDev = "a09e1931-b0bf-4961-b203-046c239ae452";
+    const web3FormsApiKey = import.meta.env.VITE_WEB3FORMS_API_KEY;
 
-    try {
-      // await axios.post('http://localhost:3001/api/save-form-data1', data);
-      setSuccess(true);
-      reset();
-    } catch (error) {
-      console.error('error while sending data', error);
-      setError(true);
-    } finally {
-      setFetch(false);
-
-      setTimeout(() => {
+    await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(
+        { ...data, access_key: web3FormsApiKey || web3FormsApiKeyDev },
+        null,
+        2
+      ),
+    })
+      .then(async (response) => {
+        let json = await response.json();
+        if (json.success) {
+          setSuccess(true);
+          setError(false);
+          setMessage(json.message);
+          e.target.reset();
+          reset();
+        } else {
+          setError(true);
+          setSuccess(false);
+          setMessage(json.message);
+        }
+      })
+      .catch((error) => {
+        setError(true);
         setSuccess(false);
-        setError(false);
-      }, 1_000);
-    }
+        setMessage("Client Error. Please check the console.log for more info");
+        console.log(error);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setSuccess(false);
+          setError(false);
+        }, 5000);
+      });
+
+    // setFetch(true);
+
+    // try {
+    //   // await axios.post('http://localhost:3001/api/save-form-data1', data);
+    //   setSuccess(true);
+    //   reset();
+    // } catch (error) {
+    //   console.error('error while sending data', error);
+    //   setError(true);
+    // } finally {
+    //   setFetch(false);
+
+    // setTimeout(() => {
+    //   setSuccess(false);
+    //   setError(false);
+    // }, 1_000);
+    // }
   }, []);
 
   return (
     <div ref={ref}>
       <Section className="about-us-section about-us-section-form">
-        <Rings className="rings rings-top" rotate={isMobile ? -25 : 0} scale={isMobile ? 0.5 : 1} />
+        <Rings
+          className="rings rings-top"
+          rotate={isMobile ? -25 : 0}
+          scale={isMobile ? 0.5 : 1}
+        />
         <h2 className="title">Leave us a message</h2>
-        <p className="description">Our team will contact you as quickly as possible</p>
+        <p className="description">
+          Our team will contact you as quickly as possible
+        </p>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-wrapper">
-            <div className={classNames('form-wrapper-inner', { 'error': isValidationError })}>
+            <div
+              className={classNames("form-wrapper-inner", {
+                error: isValidationError,
+              })}
+            >
               <Controller
                 name={EFields.NAME}
                 control={control}
-                rules={{ required: 'required' }}
+                rules={{ required: "required" }}
                 render={({ field, ...rest }) => (
                   <BaseInput
                     field={field}
@@ -98,7 +154,7 @@ export const FormSection: FC = () => {
               <Controller
                 name={EFields.COMPANY_NAME}
                 control={control}
-                rules={{ required: 'required' }}
+                rules={{ required: "required" }}
                 render={({ field }) => (
                   <BaseInput
                     field={field}
@@ -111,7 +167,7 @@ export const FormSection: FC = () => {
               <Controller
                 name={EFields.EMAIL}
                 control={control}
-                rules={{ required: 'required' }}
+                rules={{ required: "required" }}
                 render={({ field }) => (
                   <BaseInput
                     field={field}
@@ -125,7 +181,7 @@ export const FormSection: FC = () => {
                 <Controller
                   name={EFields.PHONE}
                   control={control}
-                  rules={{ required: 'required' }}
+                  rules={{ required: "required" }}
                   render={({ field }) => (
                     <BaseInput
                       field={field}
@@ -156,7 +212,7 @@ export const FormSection: FC = () => {
             <Controller
               name={EFields.MESSAGE}
               control={control}
-              rules={{ required: 'required' }}
+              rules={{ required: "required" }}
               render={({ field }) => (
                 <BaseInput
                   field={field}
@@ -170,7 +226,7 @@ export const FormSection: FC = () => {
             <Controller
               name={EFields.AGREEMENT}
               control={control}
-              rules={{ required: 'required' }}
+              rules={{ required: "required" }}
               render={({ field }) => (
                 <BaseInput
                   field={field}
@@ -193,8 +249,12 @@ export const FormSection: FC = () => {
             />
           </div>
         </form>
-        <Rings className="rings rings-bottom" rotate={-90} scale={isMobile ? 0.5 : 1} />
+        <Rings
+          className="rings rings-bottom"
+          rotate={-90}
+          scale={isMobile ? 0.5 : 1}
+        />
       </Section>
     </div>
   );
-}
+};
