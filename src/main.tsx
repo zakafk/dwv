876f546470b0d08addd5a91bba3157989b87081b
Zaqro Gogolidze <gogolidze.zaqro@gmail.com>
2024-07-22 22:52:54 +0400

    fix: vite config

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './components/App/App';
-import { MainLayout } from './components/MainLayout';
-import { Footer } from './components/Footer';
-import { Header } from './components/Header';
+import App from "./components/App/App";
+import { MainLayout } from "./components/MainLayout";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 
-import './main.scss';
+import "./main.scss";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/dwv/">
+    <BrowserRouter basename="/">
       <MainLayout>
         <Header />
         <App />
         <Footer />
       </MainLayout>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dwv/',
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
-})
+});
